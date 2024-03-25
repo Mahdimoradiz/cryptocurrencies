ba728159bdf5ffa766d516acbfbab6cdd901cd36
mahdi moradi <mm2129260@gmail.com>
2024-03-25 12:40:44 +0330

    add some details

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('coin/', views.coinPrice, name="coin"),
-    path('coin/time', views.time, name="coin"),
+    path('coin/time', views.time, name="date_now"),
 ]
